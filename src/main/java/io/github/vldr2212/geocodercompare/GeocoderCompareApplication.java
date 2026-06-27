9bbc7d99f06482e067833cabeb44a516e2a3106c
Vldr <162595708+Vldr22@users.noreply.github.com>
2026-06-27 14:17:26 +0300

    feature: первичный каркас проекта и инфраструктура (#1)

---
+++ src/main/java/io/github/vldr2212/geocodercompare/GeocoderCompareApplication.java
@@ -0,0 +1,13 @@
+package io.github.vldr2212.geocodercompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocoderCompareApplication.class, args);
+    }
+
+}
